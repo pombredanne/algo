377c7db32f3d61498d78abff56e36b43a50f5776
Lars Buitinck <larsmans@gmail.com>
2015-05-02 14:13:43 +0200

    speed up prime sieve by ×6 + fix corner case

--- math/prime.go
+++ math/prime.go
@@ -15,21 +15,26 @@ type Sieve32 struct {
 func (s *Sieve32) Next(buf []uint32) (primes []uint32) {
 	// Sieve of Eratosthenes, inspired by David Eppstein's Python version at
 	// https://code.activestate.com/recipes/117119-sieve-of-eratosthenes/
-	// (with the optimizations by Alex Martelli and Tim Hochberg).
+	// with the optimization by Alex Martelli. Further optimized by simple
+	// wheel factorization: only check 6n-1 and 6n+1.
 
 	i, n := 0, len(buf)
 	primes = buf
 	q, factors := s.next, s.factors
 
-	if factors == nil {
-		q, factors = 1, make(map[uint64]uint64)
+	if factors == nil && n > 0 {
+		factors = make(map[uint64]uint64)
 		s.factors = factors
 		primes[0] = 2
 		i++
 	}
+	if q == 0 && i < n {
+		primes[i] = 3
+		i++
+		q = 5
+	}
 
 	for q < (1<<32-3) && i < n {
-		q += 2
 		factor, composite := factors[q]
 		if composite {
 			delete(factors, q)
@@ -45,6 +50,12 @@ func (s *Sieve32) Next(buf []uint32) (primes []uint32) {
 			factors[q*q] = 2 * q
 			primes[i] = uint32(q)
 			i++
+		}
+
+		if (q+1)%6 == 0 {
+			q += 2
+		} else {
+			q += 4
 		}
 	}
 	s.next = q
--- math/prime_test.go
+++ math/prime_test.go
@@ -7,14 +7,30 @@ var first = [...]uint32{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47}
 func TestSieve(t *testing.T) {
 	var s Sieve32
 
+	// This should not panic.
+	s.Next([]uint32{})
+
 	for _, k := range []int{1, 2, 5, 8, len(first)} {
 		s = Sieve32{} // reset
 		got := make([]uint32, k)
 		s.Next(got)
+		s.Next([]uint32{})
 		for i := range got {
 			if got[i] != first[i] {
 				t.Errorf("%d'th prime is %d, got %d", i, first[i], got[i])
 			}
+		}
+	}
+
+	// Test incremental sieving
+	s = Sieve32{}
+	got := make([]uint32, len(first))
+	s.Next(got[:2])
+	s.Next(got[2:6])
+	s.Next(got[6:])
+	for i := range got {
+		if got[i] != first[i] {
+			t.Errorf("%d'th prime is %d, got %d", i, first[i], got[i])
 		}
 	}
 }
