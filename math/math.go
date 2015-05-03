ac94d4b5d6276831d2cb7069142174fa7e59a06a
Lars Buitinck <larsmans@gmail.com>
2015-05-04 00:00:22 +0200

    add math.LogAddExp

---
+++ math/math.go
@@ -0,0 +1,12 @@
+// Mathematical utilities.
+package math
+
+import "math"
+
+// log(exp(a) + exp(b)), evaluated in a numerically stable way.
+func LogAddExp(a, b float64) float64 {
+	if b > a {
+		a, b = b, a
+	}
+	return a + math.Log1p(math.Exp(b - a))
+}
---
+++ math/math_test.go
@@ -0,0 +1,15 @@
+package math
+
+import (
+	"math"
+	"testing"
+)
+
+func TestLogAddExp(t *testing.T) {
+	x := LogAddExp(1e-10, math.Inf(-1))
+	if x != 1e-10 {
+		t.Errorf("expected exactly 1e-10, got %g", x)
+	}
+	t.Logf("naïve version returns %g",
+		math.Log(math.Exp(1e-10) + math.Exp(math.Inf(-1))))
+}
