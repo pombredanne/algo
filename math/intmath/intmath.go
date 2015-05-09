ecee917d6033a362b3c4f59bbec1423be75cd639
Lars Buitinck <larsmans@gmail.com>
2015-05-09 13:20:49 +0200

    speed up Popcount32 by a factor >3
    ... on x86-64.

--- math/intmath/intmath.go
+++ math/intmath/intmath.go
@@ -18,10 +18,12 @@ func Log2(v int) int {
 }
 
 // Number of bits set in v.
-func Popcount32(v uint32) (c int) {
-	// https://graphics.stanford.edu/~seander/bithacks.html#CountBitsSetKernighan
-	for c = 0; v != 0; c++ {
-		v &= v - 1
-	}
-	return
+func Popcount32(v uint32) int {
+	// https://graphics.stanford.edu/~seander/bithacks.html#CountBitsSet64
+	// (Assumes fast 64-bit instructions.)
+	v64 := uint64(v)
+	c := ((v64 & 0xfff) * 0x1001001001001 & 0x84210842108421) % 0x1f
+	c += (((v64 & 0xfff000) >> 12) * 0x1001001001001 & 0x84210842108421) % 0x1f
+	c += ((v64 >> 24) * 0x1001001001001 & 0x84210842108421) % 0x1f
+	return int(c)
 }
--- math/intmath/intmath_test.go
+++ math/intmath/intmath_test.go
@@ -1,6 +1,9 @@
 package intmath
 
-import "testing"
+import (
+	"math/rand"
+	"testing"
+)
 
 func TestLog2(t *testing.T) {
 	for _, c := range []struct{ n, log int }{
@@ -8,6 +11,35 @@ func TestLog2(t *testing.T) {
 	} {
 		if got := Log2(c.n); got != c.log {
 			t.Errorf("expected %d, got %d", c.log, got)
+		}
+	}
+}
+
+func TestPopcount(t *testing.T) {
+	for _, c := range []struct {
+		n   uint32
+		pop int
+	}{
+		{0, 0}, {1 << 5, 1}, {1<<32 - 1, 32}, {1<<32 - 2, 31}, {69161, 7},
+	} {
+		if got := Popcount32(c.n); got != c.pop {
+			t.Errorf("expected %d, got %d", c.pop, got)
+		}
+	}
+}
+
+func BenchmarkPopcount(b *testing.B) {
+	b.StopTimer()
+	r := rand.New(rand.NewSource(747))
+	input := make([]uint32, 100000)
+	for i := range input {
+		input[i] = r.Uint32()
+	}
+
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		for _, a := range input {
+			Popcount32(a)
 		}
 	}
 }
