bd1ba75c47ec32d2bb49d3fe64c2e0a8c227407f
Lars Buitinck <larsmans@gmail.com>
2015-05-23 14:18:38 +0200

    require OutDegree method on graph.Directed
    This will allow us to distinguish directed and undirected graphs later on.

--- graph/interface.go
+++ graph/interface.go
@@ -16,11 +16,21 @@ func (g AdjacencyList) NVertices() int {
 	return len(g)
 }
 
+func (g AdjacencyList) OutDegree(u int) int {
+	return len(g[u])
+}
+
+var _ FiniteDirected = AdjacencyList{}
+var _ FiniteDirected = &AdjacencyList{}
+
 // Interface for directed graphs.
 type Directed interface {
 	// The neighbors of u. Order doesn't matter; consumer algorithms will not
 	// modify the returned slice.
 	Neighbors(u int) []int
+
+	// Out-degree of u; must match the length of Neighbors(u).
+	OutDegree(u int) int
 }
 
 // Interface for directed graphs of finite/known size.
