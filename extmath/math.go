bf127c543c44f9799699269c83bc8ad57ccb6f4c
Lars Buitinck <larsmans@gmail.com>
2015-06-11 13:24:11 +0200

    Logistic and Logit in extmath

--- extmath/math.go
+++ extmath/math.go
@@ -10,3 +10,19 @@ func LogAddExp(a, b float64) float64 {
 	}
 	return a + math.Log1p(math.Exp(b-a))
 }
+
+// Logistic function: 1/(1+exp(-x)).
+func Logistic(x float64) float64 {
+	return .5 * (1 + math.Tanh(.5*x))
+}
+
+// Logit, the inverse of the logistic function: log(p/(1-p)).
+//
+// Special cases are:
+//	Logit(1) = Inf
+//	Logit(0) = -Inf
+//	Logit(p < 0) = NaN
+//	Logit(p > 1) = NaN
+func Logit(p float64) float64 {
+	return math.Log(p/(1-p))
+}
--- extmath/math_test.go
+++ extmath/math_test.go
@@ -13,3 +13,31 @@ func TestLogAddExp(t *testing.T) {
 	t.Logf("naïve version returns %g",
 		math.Log(math.Exp(1e-10)+math.Exp(math.Inf(-1))))
 }
+
+func TestLogistic(t *testing.T) {
+	for _, x := range []float64{
+		1e-300, 1e-15, 1e-7, .2, .3, .5,
+	} {
+		for _, x := range []float64{x, -x} {
+			p := Logistic(x)
+			logit := Logit(p)
+			if err := math.Abs(logit - x); err > 3e-16 {
+				t.Errorf("mismatch: %g differs from %g by %g", Logit(p), x, err)
+			}
+		}
+	}
+
+	for _, c := range []struct{p, logit float64} {
+		{1, math.Inf(1)},
+		{0, math.Inf(-1)},
+	} {
+		if logit := Logit(c.p); logit != c.logit {
+			t.Errorf("Logit error: got %g, wanted %g", logit, c.logit)
+		}
+	}
+	for _, p := range []float64{-1, -1e300, 1+1e300, 2} {
+		if !math.IsNaN(Logit(p)) {
+			t.Errorf("expected NaN for Logit(%g), got %g", p, Logit(p))
+		}
+	}
+}
