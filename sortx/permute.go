303817806abb1d57385317e32fefb19ae7805f11
Lars Buitinck <larsmans@gmail.com>
2015-10-30 22:14:43 +0100

    sortx.PrevPermutation

--- sortx/permute.go
+++ sortx/permute.go
@@ -1,3 +1,9 @@
+// Copyright 2015 Lars Buitinck
+//
+// MIT-licensed. See the file LICENSE for details.
+
+package sortx
+
 package sortx
 
 import "sort"
@@ -16,6 +22,27 @@ func NextPermutation(data sort.Interface) bool {
 	}
 	for l = k + 1; l < n-1; l++ {
 		if data.Less(l+1, k) {
+			break
+		}
+	}
+	data.Swap(k, l)
+	reverse(data, k+1, n)
+	return true
+}
+
+// Generates the lexicographically previous multiset permutation, in-place.
+//
+// Returns false when there is no previous permutation, i.e., data is sorted.
+func PrevPermutation(data sort.Interface) bool {
+	n := data.Len()
+	var k, l int
+	for k = n - 2; k >= 0 && !data.Less(k+1, k); k-- {
+	}
+	if k < 0 {
+		return false
+	}
+	for l = k + 1; l < n-1; l++ {
+		if data.Less(k, l+1) {
 			break
 		}
 	}
--- sortx/permute_test.go
+++ sortx/permute_test.go
@@ -14,11 +14,24 @@ func TestPermute(t *testing.T) {
 	if nperm != 120 {
 		t.Errorf("%v has 120 permutations, got %d", a, nperm)
 	}
+	for PrevPermutation(a) {
+		nperm--
+	}
+	if nperm != 1 {
+		t.Error("PrevPermutation stopped after %d steps", 120-nperm)
+	}
+	if !sort.IsSorted(a) {
+		t.Error("expected data to be sorted, got %v", a)
+	}
 
 	// Multiset permutations.
 	a = sort.IntSlice{1, 2, 2}
-	for _, p := range [][]int{{2, 1, 2}, {2, 2, 1}} {
-		NextPermutation(a)
+	perms := [][]int{{1, 2, 2}, {2, 1, 2}, {2, 2, 1}}
+	for _, p := range perms[1:] {
+		ok := NextPermutation(a)
+		if !ok {
+			t.Errorf("NextPermutation returned false at %v", a)
+		}
 		for i := range a {
 			if a[i] != p[i] {
 				t.Errorf("wanted %v, got %v", p, a)
@@ -26,8 +39,19 @@ func TestPermute(t *testing.T) {
 		}
 	}
 	if NextPermutation(a) {
-		t.Errorf(`array was reverse-sorted, but got "next permutation" %v`,
-			a)
+		t.Errorf(`array was reverse-sorted, but got "next permutation" %v`, a)
+	}
+	for j := 0; j < len(perms)-1; j++ {
+		p := perms[len(perms)-j-1]
+		for i := range a {
+			if a[i] != p[i] {
+				t.Errorf("wanted %v, got %v", p, a)
+			}
+		}
+		ok := PrevPermutation(a)
+		if !ok {
+			t.Errorf("PrevPermutation returned false at %v", a)
+		}
 	}
 
 	// Corner cases.
@@ -35,5 +59,16 @@ func TestPermute(t *testing.T) {
 		if NextPermutation(a) {
 			t.Errorf(`got "next permutation" for length-%d array`, len(a))
 		}
+	}
+}
+
+func BenchmarkPrev(b *testing.B) {
+	a := make(sort.IntSlice, 1000)
+	for i := range a {
+		a[i] = -i
+	}
+
+	for i := 0; i < b.N; i++ {
+		PrevPermutation(a)
 	}
 }
